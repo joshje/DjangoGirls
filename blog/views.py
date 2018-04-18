dc00731eaf53da1d8ef0bca87c2ab9455aabffbd
Shaun Greene <shaun@spgreene.com>
2018-04-18 21:36:23 +0000

    Display blogs in reverse chronological order (newest first)

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
-    posts = posts.order_by('published_date')
+    posts = posts.order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
